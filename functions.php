22176072d7dd0944545a32ff5ad719b69097edb1
Tu Nombre <you@example.com>
2024-05-07 19:24:25 +0200

    added normalize

--- functions.php
+++ functions.php
@@ -11,3 +11,10 @@ function gymfitness_menus(){
 
 //hook
 add_action('init', 'gymfitness_menus');
+
+//add stylesheets and js
+function gymfitness_scripts(){
+    wp_enqueue_style('normalize', get_template_directory().'css/normalize.css', array(), '8.0.1');
+
+}
+add_action('wp_enqueue_scripts', 'gymfitness_scripts');
