82e8c8b6784105740b73f154e30e61e2bf5c763e
Tu Nombre <you@example.com>
2024-05-10 12:39:44 +0200

    ya tenemos la single page

--- template-parts/page-loop.php
+++ template-parts/page-loop.php
@@ -3,14 +3,25 @@
     <h1 class="text-center text-primary"><?php the_title(); ?></h1>
 
     <?php if (has_post_thumbnail()): ?>
-        <?php the_post_thumbnail('blog', array(
-            'class' => 'featured-image'
-        )
+        <?php the_post_thumbnail(
+            'blog',
+            array(
+                'class' => 'featured-image'
+            )
         ); ?>
+    <?php endif;
+    //check the current post type
+    if (get_post_type() === 'gymfitness_classes'):
+        $pod = pods('gymfitness_classes', get_the_id());
+        $start_time = $pod->field('start_time');
+        $end_time = $pod->field('end_time');
+        $class_days = $pod->field('class_days'); ?>
+        <p class="content-class"><?= $class_days ?> - <?= $start_time ?> to <?= $end_time ?></p>
 
+        <?php
+    endif;
+    ?>
 
-
-    <?php endif; ?>
 
     <div class="text-left">
         <p><?php the_content(); ?></p>
