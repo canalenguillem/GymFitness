c58ff375ee1056aedc809d13564904b7123ac3ad
Tu Nombre <you@example.com>
2024-05-11 07:39:17 +0200

    solved center things

--- single.php
+++ single.php
@@ -1,9 +1,8 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<?php while(have_posts()):the_post(); ?>
-    <h1><?php the_title();?></h1>
-    <p><?php the_content();?></p>
+<main class="container page section no-sidebar">
+    <?php get_template_part('template-parts/page', 'loop'); ?>
+</main>
 
-<?php endwhile;?>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
--- template-parts/page-loop.php
+++ template-parts/page-loop.php
@@ -3,6 +3,7 @@
     <h1 class="text-center text-primary"><?php the_title(); ?></h1>
 
     <?php if (has_post_thumbnail()): ?>
+
         <?php the_post_thumbnail(
             'blog',
             array(
@@ -22,8 +23,7 @@
     endif;
     ?>
 
-
-    <div class="text-left">
-        <p><?php the_content(); ?></p>
-    </div>
+        <div class="text-left">
+            <p><?php the_content(); ?></p>
+        </div>
 <?php endwhile; ?>
