40e21374902a243a7979b655ec699424f107a97d
Tu Nombre <you@example.com>
2024-05-11 06:58:42 +0200

    plantilla blog

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<?php get_header();?>
-
-<?php while(have_posts()):the_post(); ?>
-    <h1><?php the_title();?></h1>
-    <p><?php the_content();?></p>
-
-<?php endwhile;?>
-
-<?php get_footer();?>
+<?php echo "hello world";?>
